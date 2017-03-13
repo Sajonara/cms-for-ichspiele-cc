55527219e5a0c9759a92486ed2dd2efc35a3a67a
Alexander Trust <atrust@sajonara.de>
2017-03-13 01:12:21 +0000

    add confirmation validatio for user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, confirmation: true
   
   has_many :games
   has_many :news
