4cc86079b09a6b75714a0aac9e2bbf5a634dd1b2
Alexander Trust <atrust@sajonara.de>
2017-03-13 03:06:13 +0000

    make sure email is all lowercase before saved to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  
   validates :name, presence: true, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, confirmation: true, format: { with: VALID_EMAIL_REGEX }
