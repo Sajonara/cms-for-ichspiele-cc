a0781ba74c71a42e0b1a01c12979adc4951b6fcd
Alexander Trust <atrust@sajonara.de>
2017-03-19 04:28:57 +0000

    refactor users controller. add set_user method to populate different actions with proper data.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   
   def new
     @user = User.new
@@ -15,15 +16,14 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
+    
   end
   
   def edit
-    @user = User.find(params[:id])
+    
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Dein Profil wurde erfolgreich aktualisiert."
       redirect_to @user
@@ -32,10 +32,18 @@ class UsersController < ApplicationController
     end
   end
   
+  def index
+    @user = User.all
+  end
+  
   private
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
   
 end
