1aa5e81b3ee5ec710a424c602b8951fc7cc3f234
Alexander Trust <atrust@sajonara.de>
2017-03-20 02:18:45 +0000

    add preliminary sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   namespace :admin do
     root 'admin#index'
     get 'games/' => 'games#index'
