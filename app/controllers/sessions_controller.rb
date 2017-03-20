84d68607001599f2a1983b3cdfaa1aec9d70387f
Alexander Trust <atrust@sajonara.de>
2017-03-20 02:44:22 +0000

    add preliminary login and logout feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,21 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:succes] = "Du hast Dich eingeloggt."
+      redirect_to user
+    else
+      flash.now[:danger] = "Es ist ein Fehler beim Login aufgetreten."
+      render 'new'
+    end
   end
   
   def destroy
+    session[:user_id] = nil
+    flash[:succes] = "Du hast dich ausgeloggt."
+    redirect_to root_path
   end
   
 end
