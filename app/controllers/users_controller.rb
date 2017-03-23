1e09a4c8f19f48ebd2c19a99f0087afd569c04ef
Alexander Trust <atrust@sajonara.de>
2017-03-23 03:10:54 +0000

    log user in automatically after he signs up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Herzlich Willkommen #{@user.name} zu IchSpiele."
       redirect_to user_path(@user)
     else
